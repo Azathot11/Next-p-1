375004f39294d368bd0cf768d8519da5b94a7470
paul nji <Azathot11>
2023-12-28 22:57:25 +0100

    new changes

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,44 +1,35 @@
 import styles from './page.module.css';
 import Image from "next/image";
+import {getMeal} from "@/lib/meals";
 import {notFound} from "next/navigation";
-import {getMeals} from "@/lib/meals";
 
 
-const Meals = async () => {
-    const meals = await getMeals();
-    return meals;
-}
-const MealDetail = async({params}) => {
+const MealDetail = ({params}) => {
     const {mealSlug} = params;
-    const meals = await Meals();
-    console.log(meals)
-    // const meals = await  getMeals()
-    // const foundMeal =  meals.find(meal => meal.slug === mealSlug);
+    const foundMeal = getMeal(mealSlug);
 
-    // if (!foundMeal) {
-    //     notFound();
-    // }
-    // foundMeal.instructions = foundMeal.instructions.replace(/\n/g, '<br/>');
+    if (!foundMeal) {
+        notFound();
+    }
+    foundMeal.instructions = foundMeal.instructions.replace(/\n/g, '<br/>');
     return (
         <>
-            <p>paulio</p>
-            {/*<h1>Test</h1>*/}
-            {/*<header className={styles.header}>*/}
-            {/*    <div className={styles.image}>*/}
-            {/*        <Image src={foundMeal.image} alt={foundMeal.title} fill/>*/}
-            {/*    </div>*/}
-            {/*    <div className={styles.headerText}>*/}
-            {/*        <h1>{foundMeal.title}</h1>*/}
-            {/*        <p className={styles.creator}>by <a*/}
-            {/*            href={`mailto:${foundMeal.creator_email}`}>{foundMeal.creator}</a></p>*/}
-            {/*        <p className={styles.summary}>SUMMARY</p>*/}
-            {/*    </div>*/}
-            {/*</header>*/}
-            {/*<main>*/}
-            {/*    <p className={styles.instructions} dangerouslySetInnerHTML={{*/}
-            {/*        __html: foundMeal.instructions*/}
-            {/*    }}></p>*/}
-            {/*</main>*/}
+            <header className={styles.header}>
+                <div className={styles.image}>
+                    <Image src={foundMeal.image} alt={foundMeal.title} fill/>
+                </div>
+                <div className={styles.headerText}>
+                    <h1>{foundMeal.title}</h1>
+                    <p className={styles.creator}>by <a
+                        href={`mailto:${foundMeal.creator_email}`}>{foundMeal.creator}</a></p>
+                    <p className={styles.summary}>SUMMARY</p>
+                </div>
+            </header>
+            <main>
+                <p className={styles.instructions} dangerouslySetInnerHTML={{
+                    __html: foundMeal.instructions
+                }}></p>
+            </main>
         </>
     );
 };
