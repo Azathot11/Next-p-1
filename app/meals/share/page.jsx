799b29d82b3605dca4d2ec5b2d7ca4de1f64c5da
paul nji <Azathot11>
2024-01-18 08:34:39 +0100

    share forms

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,7 +1,11 @@
 import classes from './page.module.css';
 import ImagePicker from "@/components/meals/Image-picker";
+import {sharMeal} from "@/lib/actions";
 
 export default function ShareMealPage() {
+
+
+
     return (
         <>
             <header className={classes.header}>
@@ -11,7 +15,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form}>
+                <form className={classes.form} action={sharMeal}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -39,7 +43,7 @@ export default function ShareMealPage() {
                             required
                         ></textarea>
                     </p>
-                    <ImagePicker/>
+                    <ImagePicker name={'image'} label={'Your Image'}/>
                     <p className={classes.actions}>
                         <button type="submit">Share Meal</button>
                     </p>
--- components/meals/Image-picker.jsx
+++ components/meals/Image-picker.jsx
@@ -1,25 +1,49 @@
 "use client";
-import React, { useRef } from 'react';
+import React, { useRef,useState } from 'react';
 import styles from './Image-picker.module.css';
+import Image from "next/image";
 
 const ImagePicker = ({label,name}) => {
     const fileInputRef = useRef();
+    const [pickedImage,setPickedImage] = useState(null)
 
     const handleButtonClick = () => {
         fileInputRef.current.click();
     };
 
+    const handleImageChange = (event)=>{
+        const file=event.target.files[0];
+        if (!file) {
+            setPickedImage(null);
+            return;
+        }
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+        setPickedImage(fileReader.result)
+        }
+        fileReader.readAsDataURL(file);
+
+    }
+
     return (
         <div className={styles.picker}>
             <label htmlFor={name} className={''}>{label}</label>
             <div className={styles.controls}>
+                <div className={styles.preview}>
+                    {!pickedImage && <p>Please pick an image</p>}
+                    {pickedImage && <Image src={String(pickedImage)} alt={'Picked Image'} fill/>}
+                </div>
                 <input
                     ref={fileInputRef}
                     className={styles.input}
                     type="file"
                     id={name}
                     accept={'image/png, image/jpeg'}
-                    name={name}/>
+                    name={name}
+                    onChange={handleImageChange}
+                    required={true}
+                />
+
             </div>
             <button
                 className={styles.button}
